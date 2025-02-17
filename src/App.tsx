5fa9e1f6a20be2c38f06601b617a9d43a9ecd178
ohdaeo <assa01389@gmail.com>
2025-02-17 11:56:27 +0900

    state 의 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
+interface UserInfo {
+  id: number;
+  level: number;
+  login?: boolean;
+}
+
 function App() {
+  // 초기 값 설정
+  // 제네릭으로 타입변수 지정
+  const [count, setCount] = useState<number>(0);
+  const [point, setPoint] = useState<number | null>(0);
+
+  // 객체형 타입 정의
+  const [user, setUser] = useState<UserInfo | null>(null);
+
   return <div>App</div>;
 }
 
